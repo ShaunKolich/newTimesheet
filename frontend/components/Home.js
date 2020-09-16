dabc5ed294c284a7d79a471b38fed40b74bfdc71
unknown <globalxbank@gmail.com>
2020-09-16 10:53:45 -0400

    updating project

---
+++ frontend/components/Home.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    state = {  }
+    render() { 
+        return ( <h1>Testing Home</h1> );
+    }
+}
+ 
+export default Home;
